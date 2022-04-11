e6134a60eaa50c010982ffb9c0bf213b485332e0
Vije Miller <98605839+millworks@users.noreply.github.com>
2022-04-11 02:15:04 -0400

    Add files via upload

--- back/js/miter_form.js
+++ back/js/miter_form.js
@@ -30,27 +30,9 @@ function validate() {
 	}
 }
 
-// miter inserts
-var more_buttons_dropdown = document.getElementById('more_buttons_dropdown');
-
-more_buttons_dropdown.onchange = function() {
-    var txtarea = document.getElementById('miter');
-    var scrollPos = txtarea.scrollTop;
-    var caretPos = txtarea.selectionStart;
-    var front = (txtarea.value).substring(0, caretPos);
-    var back = (txtarea.value).substring(txtarea.selectionEnd, txtarea.value.length);
-    txtarea.value = front + this.value + back;
-    caretPos = caretPos + this.length;
-    txtarea.selectionStart = caretPos;
-    txtarea.selectionEnd = caretPos;
-    txtarea.focus();
-    txtarea.scrollTop = scrollPos;
-}
-
 // upload filename insert into field
 const uploadBTN = document.getElementById('upload');
 uploadBTN.addEventListener('change',
 function() {
     document.getElementById('url').value = this.files[0].name
 })
-
