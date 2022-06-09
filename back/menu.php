62a9f9fe85d10b1f4a1b262003396708d799359c
Vije Miller <98605839+millworks@users.noreply.github.com>
2022-06-09 00:48:11 -0400

    Add files via upload

--- back/menu.php
+++ back/menu.php
@@ -9,17 +9,15 @@
 	<table class="menu_table">
 		<tr>
 			<td class="menu_td">
-				<a href="?u=miter_images" style="color:#111111;">Miter Images</a><br />
-				<a href="?u=miter_inserts" style="color:#111111;">Miter Inserts</a><br />
 				<a href="?u=new" style="color:#111111;">New Tenon</a><br />
 				<a href="?u=tenons" style="color:#111111;">Tenon Archive</a><br />
+				<hr /><a href="?u=upload" style="color:#111111;">Upload</a><br />
 				<a href="?u=tenon_images" style="color:#111111;">Tenon Images</a><br />
+				<a href="?u=miter_images" style="color:#111111;">Miter Images</a><br />
+				<a href="?u=tenon_files" style="color:#111111;">Documents</a><br />
+				<hr /><a href="?u=miter_inserts" style="color:#111111;">Miter Inserts</a><br />
 				<a href="?u=tenon_inserts" style="color:#111111;">Tenon Inserts</a><br />
-			</td>
-			<td class="menu_td" align="right">
-				<a href="?u=upload" style="color:#111111;">Upload</a><br />
-				<a href="?u=tenon_files" style="color:#111111;">Files</a><br />
-				<a href="?u=panels" style="color:#111111;">Panels</a><br />
+				<hr /><a href="?u=panels" style="color:#111111;">Panels</a><br />
 				<a href="?u=settings" style="color:#111111;">Settings</a><br />
 				<a href="?u=help" style="color:#111111;">Help</a><br />
 				<a href="back/logout.php" style="color:#111111;">Logout</a>
