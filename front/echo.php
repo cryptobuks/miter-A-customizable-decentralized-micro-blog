e4d2324401c666a4f061829435479e7a007e7d55
Vije Miller <98605839+millworks@users.noreply.github.com>
2022-06-28 13:14:28 -0400

    Add files via upload

--- front/echo.php
+++ front/echo.php
@@ -105,7 +105,7 @@
 		}
 		
 	// image
-	} else if (strpos($last_link, '.jpg') !== false || strpos($last_link, '.jpeg') !== false || strpos($last_link, '.png') !== false || strpos($last_link, '.gif') !== false || strpos($last_link, '.svg') !== false) {
+	} else if (strpos($last_link, '.jpg') !== false || strpos($last_link, '.jpeg') !== false || strpos($last_link, '.png') !== false || strpos($last_link, '.gif') !== false || strpos($last_link, '.svg') !== false || strpos($last_link, '.webp') !== false) {
 		
 		// upload then no image
 		if (strlen($last_img) !== 0) { // null
